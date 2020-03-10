7d25babb9a5aa73d2c1be8642b563b51f1cbafb3
oyeyipo45 <oyeyipo45@gmail.com>
2020-03-10 12:27:16 +0100

    remove posts functional

--- src/app.js
+++ src/app.js
@@ -1,29 +1,64 @@
 import { http } from "./http";
 import { ui } from "./ui";
 
-//get post om DOM load
-document.addEventListener("DOMcontentloaded", getPosts);
+//Get post om DOM load
+document.addEventListener("DOMContentLoaded", getPosts);
 
-//listen for add posts
-document.querySelector("DOMcontentloaded", submitPost)
+//listen for delete
+document.querySelector('.post-submit').addEventListener("click", submitPost);
 
-//arrow functions does not work for asnyc methods
+//listen for delete
+document.querySelector('#posts').addEventListener("click", deletePost);
+
 function getPosts(){
-    
     http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
-    
 }
 
 
 //Add posts
-getPosts();
-
 function submitPost(){
     const title = document.querySelector("#title").value;
     const body = document.querySelector("#body").value;
 
-    
+    const data = {
+        //since the key and the title are thesame in ES6 you can just write the value like that
+        title: title,
+        body: body
+    }
+
+    //Create post, making a http post request
+    http.post("http://localhost:3000/posts", data)
+    .then(data => {
+        ui.showAlert('Post added', "alert alert-success");
+        ui.clearFields();
+        getPosts();
+    })
+    .catch(err => console.log(err));
 }
 
+
+//Delete Post
+function deletePost(e){
+    e.preventDefault();
+    //checking is the parent element contains a delete class so the delete effect can take place on the click
+    if(e.target.parentElement.classList.contains("delete")){
+        //creating an Id to differentiate which post you want to delete using their id in the UI
+        const id = e.target.parentElement.dataset.id;
+        //asking for confirmation of delete
+        if(confirm("Please confirm delete?")){
+            http
+            //deleting the data from the db using the ${id} as declared above
+            .delete(`http://localhost:3000/posts/${id}`)
+            //handling the promise result
+            .then(data => {
+                //the alert-success class makes it automatically a green alert
+                ui.showAlert("Posts Removed", "alert alert-success")
+                //after deleting the get ppost gets the remaining posts in the db to the ui
+                getPosts();
+            })
+            .catch(err => console.log(err));
+        }
+    }
+}
--- src/ui.js
+++ src/ui.js
@@ -4,8 +4,8 @@ class UI {
         this.titleInput = document.querySelector('#title');
         this.bodyInput = document.querySelector('#body');
         this.idInput = document.querySelector('#id');
-        this.postSubmit = document.querySelector('#post-submit');
-        this.forState = document.querySelector('.add');
+        this.postSubmit = document.querySelector('.post-submit');
+        this.forState = '.add';
     }
 
     showPosts(posts){
@@ -13,24 +13,59 @@ class UI {
 
         posts.forEach((post) => {
             output += `
-            <div class="card mb-3"> 
-                <div class="card-body">
-                    <h4 class="card-title">${post.title}</h4>
-                    <p class ="card-text"<${post.body}</p>
-                <a href="#" class="edit card-link" data-id="${post.id}">
-                    <i class="fa fa-pencil"></i>
-                </a>
+                <div class="card mb-3"> 
+                    <div class="card-body">
+                        <h4 class="card-title">${post.title}</h4>
+                        <p class ="card-text">${post.body}</p>
+                    <a href="#" class="edit card-link" data-id="${post.id}">
+                        <i class="fa fa-pencil"></i>
+                    </a>
 
-                <a href="#" class="delete card-link" data-id="${post.id}">
-                    <i class="fa fa-remove"></i>
-                </a>
-                
-                
+                    <a href="#" class="delete card-link" data-id="${post.id}">
+                        <i class="fa fa-remove"></i>
+                    </a>
+                    </div>
                 </div>
-            </div>
             `;
-            this.post.innerHTML = output;
         });
+
+        this.post.innerHTML = output;
+
+    }
+
+    
+    showAlert(message, className){
+        this.clearAlert();
+
+        //create div
+        const div = document.createElement("div");
+        //add classes
+        div.className = className;
+        //add text from message
+        div.appendChild(document.createTextNode(message));
+        //get parent
+        const container = document.querySelector(".postsContainer");
+        //get posts
+        const posts = document.querySelector('#posts');
+        //insert alert div
+        container.insertBefore(div, posts);
+
+        //timeout
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert(){
+        const currentAlert = document.querySelector(".alert");
+        if(currentAlert){
+            currentAlert.remove();
+        }
+    }
+
+    clearFields(){
+        this.titleInput.value = " ";
+        this.bodyInput.value = " ";
     }
 }
 
