9365db3b428c50d6d27a2d539d9d142d59f15bc5
oyeyipo45 <oyeyipo45@gmail.com>
2020-03-10 09:04:38 +0100

    get post functional

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import { http } from "./http";
 import { ui } from "./ui";
 
-//get post om dom load
+//get post om DOM load
 document.addEventListener("DOMcontentloaded", getPosts);
+
+//listen for add posts
+document.querySelector("DOMcontentloaded", submitPost)
 
 //arrow functions does not work for asnyc methods
 function getPosts(){
@@ -13,7 +16,14 @@ function getPosts(){
     
 }
 
+
+//Add posts
 getPosts();
 
+function submitPost(){
+    const title = document.querySelector("#title").value;
+    const body = document.querySelector("#body").value;
 
+    
+}
 
